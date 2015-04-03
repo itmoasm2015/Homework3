0269fc0622ee12944fda8321ae726fa18c6c9e6f
NoNick <aleksandrs634@gmail.com>
2015-04-03 14:10:59 +0000

    Khimulya initial

---
+++ 2537/Khimulya/hw_bigint.asm
@@ -0,0 +1,51 @@
+global biFromInt
+global biFromString
+global biToString
+global biDelete
+global biSign
+global biAdd
+global biSub
+global biMul
+global biDivRem
+global biCmp
+
+;BigInt biFromInt(int64_t x);
+biFromInt:
+    ret
+
+;BigInt biFromString(char const *s);
+biFromString:
+    ret
+
+;void biToString(BigInt bi, char *buffer, size_t limit);
+biToString:
+    ret
+
+;void biDelete(BigInt bi);
+biDelete:
+    ret
+
+;int biSign(BigInt bi);
+biSign:
+    ret
+
+;void biAdd(BigInt dst, BigInt src);
+biAdd:
+    ret
+
+;void biSub(BigInt dst, BigInt src);
+biSub:
+    ret
+
+;void biMul(BigInt dst, BigInt src);
+biMul:
+    ret
+
+;void biDivRem(BigInt *quotient, BigInt *remainder, BigInt numerator, BigInt denominator);
+biDivRem:
+    ret
+
+;int biCmp(BigInt a, BigInt b);
+biCmp:
+    ret
+
