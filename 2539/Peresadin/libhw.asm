42b6b26058c7d8d1b0d838fb5cd9aeafd95b5524
Ilya Peresadin <pva701@gmail.com>
2015-04-17 00:26:29 +0300

    added vector

--- 2539/Peresadin/libhw.asm
+++ 2539/Peresadin/libhw.asm
@@ -14,59 +14,188 @@ global biMul
 global biCmp
 global biSign
 
-;Структура матрицы
-struc BigInt
-    sign:     resq 1
-    len:      resq 1
-    data:     resq 1;элементы матрицы
+BASE equ 1000000000
+
+global newVector
+global pushBack
+global popBack
+global element
+global deleteVector
+
+struc VectorInt
+    sz:        resq 1
+    alignSize: resq 1
+    elem:      resq 1;элементы вектора
 endstruc
 
-biFromInt:
+newVector:
+    mov rax, 1
+    .loop
+        shl rax, 1
+        cmp rdi, rax
+        jae .loop
+    push rax
     push rdi
-    push rbx
+    mov rdi, VectorInt_size
+    call malloc
+    pop rdi
+    mov [rax + sz], rdi
+    mov rdx, rax
+    pop rax
+    mov [rdx + alignSize], rax
+    push rdx
+    mov rdi, rax
+    shl rdi, 2
+    call malloc
+    pop rdx
+    mov [rdx + elem], rax
+    mov rax, rdx
+
+    mov rcx, [rax + sz]
+    shl rcx, 2
+    cmp rcx, 0
+    je .size_zero
+    mov rdx, [rax + elem]
+    .loop_set
+        mov dword [rdx + rcx - 4], 0
+        sub rcx, 4
+        jnz .loop_set
+
+    .size_zero
+    ret
+
+copyElem:
+    push rdi
+    mov [rdi + alignSize], rsi
+    mov rdi, rsi
+    shl rdi, 2
+    call malloc
+    pop rdi
+
+    mov rcx, [rdi + sz]
+    shl rcx, 2
+    cmp rcx, 0
+    je .size_zero
+    mov rdx, [rdi + elem]
+    .loop_copy
+        mov esi, [rdx + rcx - 4]
+        mov [rax + rcx - 4], esi
+        sub rcx, 4
+        jnz .loop_copy
+    .size_zero
+
+    push rdi
+    push rax
+    mov rdi, [rdi + elem]
+    call free
+    pop rax
+    pop rdi
+    mov [rdi + elem], rax
+    ret
+
+pushBack:
+    mov rax, [rdi + alignSize]
+    cmp [rdi + sz], rax
+    jne .push_back
+    ;align size
+        push rsi
+        mov rsi, [rdi + alignSize]
+        shl rsi, 1
+        call copyElem
+        pop rsi
+    .push_back
+    mov rax, [rdi + sz]
+    mov rdx, [rdi + elem]
+    mov [rdx + 4*rax], esi
+    inc qword [rdi + sz]
+    ret
+
+popBack:
+    dec qword [rdi + sz]
+    mov rax, [rdi + sz]
+    shl rax, 2
+    cmp rax, [rdi + alignSize]
+    ja .not_copy
+        mov rsi, [rdi + alignSize]
+        shr rsi, 1
+        call copyElem
+    .not_copy
+    ret
+
+deleteVector:
+    push rdi
+    mov rdi, [rdi + elem]
+    call free
+    pop rdi
+    call free
+    ret
+
+%macro element 3;to vec index
+    push rbp
+    mov %1, [%2 + vec]
+    mov rbp, [%1 + elem]
+    mov %1, [rbp + 4*%3]
+    pop rbp
+%endmacro
+
+%macro setElement 3;vec index x
+    push rbp
+    mov rbp, [%1 + vec]
+    mov rbp, [rbp + elem]
+    mov [rbp + 4*%2], %3
+    pop rbp
+%endmacro
+
+;Структура длинного числа
+struc BigInt
+    sign:     resq 1
+    vec:      resq 1
+endstruc
+
+%macro length 2;to vec
+    push rbp
+    mov rbp, [%2 + vec]
+    mov %1, [rbp + sz]
+    pop rbp
+%endmacro
+
+biFromInt:
     push rdi
     mov rdi, BigInt_size
     call malloc
     push rax
     mov rdi, 1
-    call malloc
+    call newVector
     pop rdx
-    mov [rdx + data], rax
+    mov [rdx + vec], rax
     mov rax, rdx
     pop rdi
 
-    mov qword [rax + len], 1
     cmp rdi, 0
     js .minus
     ;plus
         mov qword [rax + sign], 1
-        mov rbx, [rax + data]
-        mov [rbx], rdi
         jmp .sign_done
     .minus
         mov qword [rax + sign], 0
         imul rdi, -1
-        mov rbx, [rax + data]
-        mov [rbx], rdi
     .sign_done
-    pop rbx
-    pop rdi
+    setElement rax, 0, rdi
     ret
 
 cmpData:
     push rbx
-    mov rax, [rsi + len]
-    mov [rdi + len], rax
+    length rax, rdi
+    length rbx, rsi
+    cmp rax, rbx
     ja .more
     jb .less
     ;lens equal
-        mov rcx, [rdi + len]
+        mov rcx, rax
         dec rcx
         .cmp_loop
-            mov rax, [rdi + data]
-            mov rax, [rax + 8 * rcx]
-            mov rbx, [rsi + data]
-            mov rbx, [rbx + 8 * rcx]
+            element rax, rdi, rcx
+            element rbx, rsi, rcx
             cmp rax, rbx
             ja .more
             jb .less
@@ -112,20 +241,24 @@ biCmp:
 biSign:
     cmp qword [rdi + sign], 0
     je .minus
-        cmp qword [rdi + len], 1
+        mov rdi, [rdi + vec]
+        cmp qword [rdi + sz], 1
         je .len_eq1
             mov eax, 1
             jmp .sign_done
         .len_eq1
-        mov rax, [rdi + data]
-        cmp qword [rax], 0
-        je .zero
-            mov eax, 1
-            jmp .sign_done
+            mov rax, [rdi + elem]
+            cmp qword [rax], 0
+            je .zero
+                mov eax, 1
+                jmp .sign_done
         .zero
             xor eax, eax
             jmp .sign_done
     .minus
         mov eax, -1
     .sign_done
+    ret
+
+biAdd:
     ret
--- 2539/Peresadin/tests.cpp
+++ 2539/Peresadin/tests.cpp
@@ -4,48 +4,13 @@
 #include <cstdlib>
 #include <ctime>
 #include <cassert>
-#include "vectorint.h"
 #include <vector>
 #include <iostream>
 using namespace std;
-const int T = 30000000;
 
 int main() {
-    int l = clock();
-    vector <int> a;
-    for (int i = 0; i < T; ++i)
-        a.push_back(i);
-    int sum = 0;
-    for (int i = 0; i < T; ++i)
-        sum += a[i];
-    for (int i = 0; i < T; ++i)
-        a.pop_back();
-    cout << 1.0 * (clock() - l) / CLOCKS_PER_SEC << endl;
-/*    VectorInt a = newVector(10);
-    vector <int> b(10);
-    for (int i = 0; i < T; ++i) {
-        pushBack(a, i);
-        b.push_back(i);
-    }
-
-    for (int i = 0; i < b.size(); ++i) {
-        int bi = b[i];
-        int ai = element(a, i);
-        assert(bi == ai);
-    }
-
-    for (int i = 0; i < 3 * T / 4; ++i) {
-        popBack(a);
-        b.pop_back();
-    }
-
-    for (int i = 0; i < b.size(); ++i) {
-        int bi = b[i];
-        int ai = element(a, i);
-        assert(bi == ai);
-    }
-
-    pushBack(a, 1);
-    deleteVector(a);*/
+    BigInt a = biFromInt(-5);
+    BigInt b = biFromInt(-5);
+    printf("cmp = %d\n", biCmp(a, b));
     return 0;
 }
