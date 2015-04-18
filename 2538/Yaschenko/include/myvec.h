0337933e8708b0840ebd0eb76fe9f2b6362cd21e
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-04-18 17:54:26 +0300

    Add some new functions to myvec.h

--- 2538/Yaschenko/include/myvec.h
+++ 2538/Yaschenko/include/myvec.h
@@ -9,22 +9,29 @@ typedef void* Vector;
 extern "C" {
 #endif
 
-/** Creates a vector that can hold \size elements.
-*/
+/** Creates a vector that can hold \size elements. */
 Vector vectorNew(size_t size);
 
-/** Deletes vector \v.
-*/
+/** Deletes vector \v. */
 void vectorDelete(Vector v);
 
-/** Adds \element at the end of vector.
- * 	Vector automatically grows to hold elements.
- */
-void vectorPushBack(unsigned element);
+/** Adds \element at the end of vector. Vector automatically grows to hold elements. */
+void vectorPushBack(Vector v, unsigned element);
 
+/** Returns index'th element of vector \v, or 0 if index is out of bounds. */ 
 unsigned vectorGet(Vector v, size_t index);
 
+/** Returns last element of VECTOR, or zero if vector is empty. */
+unsigned vectorBack(Vector v);
+
+/** Sets \index'th element of vector v to value \element, or 0 if \index is out of bounds. */
 void vectorSet(Vector v, size_t index, unsigned element);
+
+/** Returns size of vector \v. */
+size_t vectorSize(Vector v);
+
+/** Returns capacity of VECTOR. */
+size_t vectorCapacity(Vector v);
 
 #ifdef __cplusplus
 }
