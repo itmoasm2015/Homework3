9790d400c91e1317483200758c28826ea130fea8
NoNick <aleksandrs634@gmail.com>
2015-04-21 11:18:05 +0000

    biSign implemented

--- 2537/Khimulya/hw_bigint.asm
+++ 2537/Khimulya/hw_bigint.asm
@@ -54,22 +54,68 @@ endstruc
         call    free
 %endmacro
 
+; returns 0 if given bigint is positive and -1 otherwise
+;
+; corrupts r8, r9
+;
+; @arg %1 address of bigint
+;
+; @return %2 64-bit representation of 0 or -1
+%macro  SIGN 2
+        mov     r8, [%1 + bigint.len]
+        mov     r9, [%1 + bigint.data]
+        mov     r9, [r9 + r8 - 8]
+        test    r9, [MSB]             ; last number
+        jnz     %%neg
+        mov     %2, 0
+        jmp     %%done
+     %%neg:
+        mov     %2, MINUS1_64
+     %%done:
+%endmacro
+
+; computes maximum of two general purpose registers
+;
+; @arg %2 first operand
+; @arg %3 second operand
+; note: only one of two operands can be memory
+;
+; @return %1 maxumum of given values
+%macro  MAX 3
+        sub     %2, %3
+        jo      %%second
+        mov     %1, %2
+        jmp     %%done
+    %%second:
+        mov     %1, %3
+    %%done:
+%endmacro
+
+section .rodata
+        align   8
+    MSB:
+        dq      0x8000000000000000  ; only first MSB is true
+    MINUS1_64:
+        dq      0xffffffffffffffff
+
+section .text
+
 ;BigInt biFromInt(int64_t x);
 ;
 ; @return rax address of brand new bigint struc
 biFromInt:
-        mov     rsi, 8
         push    rdi
+        mov     rsi, 8
         NEW_BI  rsi, rdx
-        pop     rcx
-        mov     [rdx], rcx
+        pop     rdi
+        mov     [rdx], rdi
         ret
 
 ;BigInt biFromString(char const *s);
 ; TODO:
 ; lodsb loads next byte from rsi address into al and increments rsi
 ; movdqa loads an double quadword integer from xmmN to mem128 and vice versa
-; pmulhw multiplies packed signed integer and stores hight result, pmullw stores low result
+; pmulhw multiplies packed signed integer and stores high result, pmullw stores low result
 biFromString:
         ret
 
@@ -83,11 +129,53 @@ biDelete:
         ret
 
 ;int biSign(BigInt bi);
-biSign:
+; Consider a few clauses and return -1, 0, 1 if number negative, zero or positive, respectly.
+; note: SIGN macro isn't applicatable here because it has only two return values
+biSign:        
+        mov     rcx, [rdi + bigint.len]
+        mov     rdx, [rdi + bigint.data]
+        mov     rdx, [rdx + rcx - 8]                    ; less significant quadword
+        test    rdx, [MSB]                              ; if MSB is set, return -1
+        jnz     .neg
+        cmp     rcx, 8                                  ; if size == 8 bytes
+        jne     .pos                                    ; and only number is zero
+        cmp     rdx, 0                                  ; return 0
+        jne     .pos
+        mov     rax, 0
+        ret
+    .pos:
+        mov     rax, 1                                  ; otherwise return 1
+        ret
+    .neg:
+        mov     rax, -1
         ret
 
 ;void biAdd(BigInt dst, BigInt src);
 biAdd:
+        mov     rdx, [rdi + bigint.len]
+        MAX     rcx, rdx, [rsi + bigint.len]
+        add     rcx, 8
+        NEW_BI  r8, rax                     ; let's allocate memory for the biggest number possible
+
+        mov     rdx, [rdi + bigint.len]
+        cmp     rdx, [rsi + bigint.len]
+        jae     .done_swap
+        xchg    rdi, rsi
+     .done_swap:                            ; now the longest number in rdi
+        mov     rcx, [rsi + bigint.len]
+        shr     rcx, 3                      ; number of iterations for loop1
+        mov     rbx, [rdi + bigint.data]
+        mov     rdx, [rsi + bigint.data]
+        clc
+        .loop1:
+                mov     r9, [rbx]
+                adc     r9, [rdx]
+                mov     [rax], r9
+                add     rbx, 8
+                add     rdx, 8
+                add     rax, 8
+                loop    .loop1
+
         ret
 
 ;void biSub(BigInt dst, BigInt src);
--- 2537/Khimulya/test.cpp
+++ 2537/Khimulya/test.cpp
@@ -12,3 +12,22 @@ TEST(correctness, allocAndFree) {
     BigInt bi = biFromInt(100);
     biDelete(bi);
 }
+
+TEST(correctness, sign) {
+    BigInt bi = biFromInt(1);
+    ASSERT_GT(biSign(bi), 0);
+    biDelete(bi);
+    int64_t tmp = 9223372036854775807l;
+    bi = biFromInt(9223372036854775807l);    // 2 ** 63 - 1
+    ASSERT_GT(biSign(bi), 0);
+    biDelete(bi);
+    bi = biFromInt(-1);
+    ASSERT_LT(biSign(bi), 0);
+    biDelete(bi);
+    bi = biFromInt(-9223372036854775808l);    // -2 ** 63
+    ASSERT_LT(biSign(bi), 0);
+    biDelete(bi);
+    bi = biFromInt(0);
+    ASSERT_EQ(biSign(bi), 0);
+    biDelete(bi);
+}
