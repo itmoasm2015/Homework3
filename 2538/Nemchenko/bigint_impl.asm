6fc87509d04602264f9dcad1a51151b1801efd3d
jon536 <jon-536@mail.ru>
2015-04-29 11:44:09 +0300

    add push_back and allocate_memory

--- 2538/Nemchenko/bigint_impl.asm
+++ 2538/Nemchenko/bigint_impl.asm
@@ -14,14 +14,71 @@ global biSub
 global biMul
 global biDivRem
 global biCmp
+;global main
+;main:
+    ;ret
 
-extern calloc
-extern free
+extern calloc, free
 
+BASE equ 1 << 64
+DEFAULT_SIZE equ 10
+
+;
+; stored bigNumber like this:
+; struct bigNum {
+;   unsigned int capacity;              4 bytes
+;   unsigned int size;                  4 bytes
+;   int sign;                           4 byte
+;   padding 4 bytes to stored "digits" in address multiples 8
+;   unsigned int64_t digits[capacity];  8 * capacity bytes
+; }
+;  
+; forall i < capacity: digits[i] < BASE
+; sign = 1 | 0 | -1 , more than 0, equal and less respectively
+
+; allocate (%1 + 2) * 8 bytes
+; pointer saved to rax
+%macro allocate_memory 1
+    push rdi
+    push rsi
+
+    mov rdi, %1
+    add rdi, 2     ; for storing capacity, size, sign. 12 bytes + 4b padding
+    mov rsi, 8
+    call calloc 
+
+    imul rdi, 8
+    mov [rax], rdi ; set capacity = (%1 + 2) * 8
+
+    pop rsi
+    pop rdi
+%endmacro
+
+; first argument - pointer to the structure bigNum
+; second argument - arg::(unsigned long long)  which will be pushed into the "digits"
+; pre: "size" have to < "capacity"
+%macro push_back 2
+    push %1
+    push rcx
+
+    xor  rcx, rcx
+    add  %1, 4            ; %1 refer to size
+    mov  ecx, dword [%1]  ; rcx = size
+    imul rcx, 8           ; rcx = size * 8
+    add  rcx, 8           ; skip sign field and padding 4 bytes
+    inc  dword [%1]       ; size++
+
+    add %1, rcx           ; %1 refer to last free position in digits
+    mov qword [%1], %2    ; set first digit to appropriate position
+
+    pop rcx
+    pop %1
+%endmacro
 
 ; BigInt biFromInt(int64_t x);
 biFromInt:
-    mov rax, rdi
+    allocate_memory DEFAULT_SIZE
+    push_back rax, rdi
     ret
 
 ; BigInt biFromString(char const *s);
--- 2538/Nemchenko/main.cpp
+++ 2538/Nemchenko/main.cpp
@@ -4,8 +4,22 @@
 
 using namespace std;
 
+void printbBigNum(void* x) {
+    int* res = (int*) x;
+    int size;
+    cout << "capacity: " << *(res++) << endl; 
+    cout << "size: " << (size = *(res++)) << endl; 
+    cout << "sign: " << *(res++) << endl; 
+    unsigned long long* res2 = (unsigned long long*) (++res);
+    
+    for (int i = 0; i < size; ++i) {
+        cout << "dig[" << i << "] = " << *(res2 + i) << endl;
+    }
+}
+
 int main() {
-    void* res = biFromInt(10);      
-    cout << (int64_t) res << endl;
+    int64_t t = 1ULL << 63;
+    printbBigNum(biFromInt(t));
+    
     return 0;
 }
